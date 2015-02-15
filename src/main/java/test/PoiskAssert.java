cb0816d26eb5826328be2e2c9e2dd1f620da32ad
iglushkov <iglushkov.v@gmail.com>
2015-02-16 01:01:51 +0200

    Added txt file

---
+++ src/main/java/test/PoiskAssert.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PoiskAssert {
+
+    @Test
+    public void SearchsomeTest() throws IOException {
+        String b = "qwe";
+        String a = new String(Files.readAllBytes(Paths.get("/Users/admin/Documents/Java_projects/Bionic-QS8/src/main/java/test/12.txt")));
+        Assert.assertTrue(a.contentEquals(b));
+    }
+}
